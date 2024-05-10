f705d2c0afb7f2cba60cc8f4c43fa11302f14771
Abdelnour <ab.fellah@esi-sba.dz>
2024-05-10 08:41:13 +0100

    updating evaluation notebook with the latest mdel

--- src/training/train.sh
+++ src/training/train.sh
@@ -1,1 +1,3 @@
-python3 train.py --dataset=flickr30k --weights-folder=transformer --histories-folder=transformer --epochs=5
+python3 train.py --dataset=flickr30k --weights-folder=transformer --histories-folder=transformer --epochs=5 --learning-rate=1e-4
+python3 train.py --dataset=flickr30k --weights-folder=transformer --histories-folder=transformer --epochs=3 --learning-rate=1e-5
+python3 train.py --dataset=flickr30k --weights-folder=transformer --histories-folder=transformer --epochs=3 --learning-rate=1e-5
